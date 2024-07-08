adee8cc6116881f1c680f4bf0a29f2bc11f30aac
Simon Shrestha <simonshrestha4@gmail.com>
2024-07-08 17:58:28 +0545

    first commit

---
+++ app/src/main/java/com/example/midterm/FragmentTwo.java
@@ -0,0 +1,46 @@
+package com.example.midterm;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentTwo#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentTwo extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private String receivedValue;
+
+    public static FragmentTwo newInstance(String param1) {
+        FragmentTwo fragment = new FragmentTwo();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            receivedValue = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+        TextView textView = view.findViewById(R.id.home);
+        if (receivedValue != null) {
+            textView.setText(receivedValue);
+        }
+        return view;
+    }
+}
